dceaeff61e81deb41adeaadad521e75e4623bc75
Kom Sihon <rsihon@gmail.com>
2019-10-22 18:20:59 +0100

    Sync Prod version and Git version

--- conf/urls.py
+++ conf/urls.py
@@ -3,14 +3,17 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from django.contrib.auth.decorators import permission_required, user_passes_test
-from ikwen.accesscontrol.utils import is_staff
-from ikwen_kakocase.shopping.views import Home, FlatPageView
 
-from ikwen_kakocase.trade.views import RetailerDashboard, LogicomDashboard
-from ikwen_kakocase.kakocase.views import MerchantList, AdminHome
+from ikwen.accesscontrol.utils import is_staff
+from ikwen.core.views import Offline
+
+from ikwen_kakocase.shopping.views import FlatPageView
+from ikwen_kakocase.kakocase.views import Welcome, AdminHome
+from ikwen_kakocase.trade.provider.views import ProviderDashboard, CCMDashboard
+
 from ikwen_webnode.webnode.views import Home
 
-from ikwen_kakocase.trade.provider.views import ProviderDashboard, CCMDashboard
+from yamo.views import save_ghost_user
 
 admin.autodiscover()
 
@@ -31,22 +34,27 @@ urlpatterns = patterns(
     url(r'^items/', include('ikwen_webnode.items.urls', namespace='items')),
     url(r'^web/', include('ikwen_webnode.web.urls', namespace='web')),
     url(r'^blog/', include('ikwen_webnode.blog.urls', namespace='blog')),
+    url(r'^revival/', include('ikwen.revival.urls', namespace='revival')),
     url(r'^rewarding/', include('ikwen.rewarding.urls', namespace='rewarding')),
+    url(r'^daraja/', include('daraja.urls', namespace='daraja')),
 
     url(r'^ikwen/dashboard/$', permission_required('trade.ik_view_dashboard')(ProviderDashboard.as_view()), name='dashboard'),
     url(r'^ikwen/CCMDashboard/$', permission_required('trade.ik_view_dashboard')(CCMDashboard.as_view()), name='ccm_dashboard'),
     url(r'^ikwen/theming/', include('ikwen.theming.urls', namespace='theming')),
     url(r'^cci/', include('ikwen_kakocase.cci.urls', namespace='cci')),
     url(r'^ikwen/cashout/', include('ikwen.cashout.urls', namespace='cashout')),
-    url(r'^ikwen/', include('ikwen.core.urls', namespace='ikwen')),
     url(r'^ikwen/home/$', user_passes_test(is_staff)(AdminHome.as_view()), name='admin_home'),
+    url(r'^ikwen/', include('ikwen.core.urls', namespace='ikwen')),
 
     url(r'^echo/', include('echo.urls', namespace='echo')),
 
     # url(r'^$', ProviderDashboard.as_view(), name='admin_home'),
     url(r'^page/(?P<url>[-\w]+)/$', FlatPageView.as_view(), name='flatpage'),
+    url(r'^welcome/$', Welcome.as_view(), name='welcome'),
 
     url(r'^home/', include('ikwen_webnode.webnode.urls', namespace='webnode')),
     url(r'^$', Home.as_view(), name='home'),
+    url(r'^offline.html$', Offline.as_view(), name='offline'),
+    url(r'^save_ghost_user$', save_ghost_user, name='save_ghost_user'),
 )
 
--- yamo/views.py
+++ yamo/views.py
@@ -1,30 +1,71 @@
 from threading import Thread
+from django.http import HttpResponse
+import json
+from django.conf import settings
 
 from django.core.mail import EmailMessage
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from django.utils.http import urlquote
 from django.views.generic import TemplateView
 from django.utils.translation import gettext as _
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
 
+from ikwen.accesscontrol.forms import MemberForm, PasswordResetForm, SMSPasswordResetForm, SetPasswordForm, \
+    SetPasswordFormSMSRecovery
+from django.http.response import HttpResponseRedirect, HttpResponse, HttpResponseForbidden
+
+from ikwen.accesscontrol.utils import invite_member
+from ikwen.core.models import Service
 from ikwen.core.constants import MALE, FEMALE
 from ikwen.core.utils import get_service_instance, get_mail_content
-from ikwen.accesscontrol.models import Member
-from ikwen.revival.models import ProfileTag, MemberProfile
-from ikwen.rewarding.models import Reward, ReferralRewardPack, CouponSummary
-from ikwen.rewarding.utils import reward_member
+from ikwen.accesscontrol.models import Member, DEFAULT_GHOST_PWD
+from ikwen.revival.models import ProfileTag, MemberProfile, Revival
+from ikwen.revival.utils import set_profile_tag_member_count
+from ikwen.rewarding.models import Reward, ReferralRewardPack, CouponSummary, Coupon, CumulatedCoupon, CROperatorProfile
+from ikwen.rewarding.utils import reward_member, JOIN
 from ikwen.accesscontrol.backends import UMBRELLA
+from ikwen_webnode.web.models import Banner, SLIDE, HomepageSection
 
 
 class Home(TemplateView):
+    template_name = 'webnode/improve/home.html'
+
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
+        context['slideshow'] = Banner.objects.filter(display=SLIDE, is_active=True).order_by('order_of_appearance')
+        context['homepage_section_list'] = HomepageSection.objects.filter(is_active=True).order_by('order_of_appearance')
+        referrer_id = self.request.GET.get('referrer')
         member = self.request.user
         service = get_service_instance()
-        # url = service.url + reverse('ikwen:company_profile', args=(project_name_slug, ))
+
+        coupon_qs = Coupon.objects.filter(status=Coupon.APPROVED, is_active=True)
         if member.is_authenticated():
-            url += '?referrer=' + member.id
+            url = 'http://tchopetyamo.com/?referrer=' + member.id
+            context['url'] = urlquote(url)
+            coupon_list = []
+            for coupon in coupon_qs:
+                try:
+                    cumul = CumulatedCoupon.objects.get(coupon=coupon, member=member)
+                    coupon.count = cumul.count
+                    coupon.ratio = float(cumul.count) / coupon.heap_size * 100
+                except CumulatedCoupon.DoesNotExist:
+                    coupon.count = 0
+                    coupon.ratio = 0
+                coupon_list.append(coupon)
             coupon_summary_list = member.couponsummary_set.filter(service=service)
             context['coupon_summary_list'] = coupon_summary_list
+            context['coupon_list'] = coupon_list
+        else:
+            context['coupon_list'] = coupon_qs
+        if referrer_id:
+            try:
+                referrer = Member.objects.get(pk=referrer_id)
+            except Member.DoesNotExist:
+                context['referrer'] = "Unknown user"
+            else:
+                context['referrer'] = referrer.get_short_name
         return context
 
 
@@ -63,3 +104,61 @@ def reward_referrer(request, *args, **kwargs):
             msg = EmailMessage(referrer_subject, html_content, sender, [referrer.email])
             msg.content_subtype = "html"
             Thread(target=lambda m: m.send(), args=(msg, )).start()
+
+
+def save_ghost_user(request, *args, **kwargs):
+    email = request.GET.get('email')
+    service = get_service_instance()
+    if email:
+        try:
+            member = Member.objects.filter(email=email)[0]
+            response = {'error': _("Email already existing; Thanks for trying.")}
+            invite_member(service, member)
+            return HttpResponse(json.dumps({'response':  response}), 'content-type: text/json')
+        except:
+            pass
+
+        username = email
+        member = Member.objects.create_user(username, DEFAULT_GHOST_PWD, email=email, is_ghost=True)
+        tag = JOIN
+        join_tag, update = ProfileTag.objects.get_or_create(name=tag, slug=tag, is_auto=True)
+
+        member_profile = MemberProfile.objects.get(member=member)
+        member_profile.save()
+
+        service = Service.objects.using(UMBRELLA).get(pk=getattr(settings, 'IKWEN_SERVICE_ID'))
+        Revival.objects.using(UMBRELLA).get_or_create(service=service, model_name='core.Service', object_id=service.id,
+                                                      mail_renderer='ikwen.revival.utils.render_suggest_create_account_mail',
+                                                      profile_tag_id=join_tag.id, get_kwargs='ikwen.rewarding.utils.get_join_reward_pack_list')
+
+        Thread(target=set_profile_tag_member_count).start()
+        response = {'response': _("Email successful created. Thanks for your concern")}
+        return HttpResponse(json.dumps({'response': response}), 'content-type: text/json')
+
+
+class JoinGP(TemplateView):
+    template_name = 'webnode/improve/join_gp.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(JoinGP, self).get_context_data(**kwargs)
+        return context
+
+
+def join_gp_now(request, *args, **kwargs):
+    member = request.user
+    if request.user.is_authenticated():
+        add_database_to_settings('gimipowa')
+        try:
+            Member.objects.using('gimipowa').get(username=member.username)
+        except Member.DoesNotExist:
+            member.save(using='gimipowa')
+            next_url = reverse('shopping:home')
+            final_url = '%s?gimipowa=joined'% (next_url)
+        else:
+            next_url = reverse('shopping:home')
+            final_url = '%s?alreadyExist=yes'% (next_url)
+    else:
+        next_url = reverse('ikwen:sign_in')
+        gp_url = reverse('join_gimipowa')
+        final_url = '%s?next=%s'% (next_url, gp_url)
+    return HttpResponseRedirect(final_url)
