0292723ddf01d01721a11dbb2cd4a9906aea11c4
W1773ND <wilfriedwillend@gmail.com>
2020-04-08 11:45:36 +0100

    Minor UX/UI fix

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -8,13 +8,18 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/w177/MEGAsync/PycharmProjects/Tchopetyamo/'
+if path not in sys.path:
+    sys.path.append(path)
 
 from conf import monitor
-
-monitor.start(interval=1.0)
-monitor.track(os.path.join(os.path.dirname(__file__)))
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+monitor.start(interval=1.0)
+monitor.track(os.path.join(os.path.dirname(__file__)))
